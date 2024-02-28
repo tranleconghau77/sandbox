222413324433edf47acb7225db50dc88fecf1a11
Hau Tran <tranleconghau772001@gmail.com>
2024-02-28 22:43:02 +0700

    store component style file next to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
+import CoreConcept from "./components/CoreConcept";
+import Header from "./components/Header/Header";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescription = ["Fundamental", "Crucial", "Core"];
-
-function getRanDomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescription[getRanDomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt="..." />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css"
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function getRanDomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescription[getRanDomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
