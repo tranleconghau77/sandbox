ba8e4dd4d140e03c617bbf0fcc76a5baf50f64a2
Hau Tran <tranleconghau772001@gmail.com>
2024-02-28 07:52:04 +0700

    use dynamic html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import reactImg from './assets/react-core-concepts.png'
+
+const reactDescription = ['Fundamental', 'Crucial', 'Core']
+
+function getRanDomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
+
+  const description = reactDescription[getRanDomInt(2)]
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
