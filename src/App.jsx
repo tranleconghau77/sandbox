54a6e7f1d48cde246d30978f1de236c65980e132
Hau Tran <tranleconghau772001@gmail.com>
2024-02-29 08:15:52 +0700

    render content conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header/Header";
 import TabButton from "./components/TabButton";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import { useState } from "react";
 import React from "react";
 
 function App() {
   // let tabContent = 'Please click a button'
-  const [tabContent, setTabContent] = useState('Please click a button text')
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
-    // console.log("Hello world!", selectedButton)
-    setTabContent(selectedButton)
+    console.log("Hello world!", selectedButton);
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -30,13 +30,22 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          {!selectedTopic && <p>Please select a topic</p>}
+          {selectedTopic && <div className="tab">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>}
         </section>
-        {tabContent}
       </main>
     </div>
   );
