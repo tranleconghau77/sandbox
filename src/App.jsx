81c13fbc559ad86b7dcb8e4a4cbd564bfd8e97eb
Hau Tran <tranleconghau772001@gmail.com>
2024-02-29 07:09:26 +0700

    understand why not to update ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header/Header";
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
+  let tabContent = 'Please click a button'
+
+  function handleSelect(selectedButton) {
+    // console.log("Hello world!", selectedButton)
+    tabContent = selectedButton
+  }
+
   return (
     <div>
       <Header />
@@ -16,6 +24,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+          </menu>
+        </section>
+        {tabContent}
       </main>
     </div>
   );
